1e60f0c3348bd1576c59871d31e280d5fc7832ac
Sumit00s <sonawanesumit245@gmail.com>
2024-10-29 00:27:07 +0530

    Added Dashoboard btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button";
+import Link from 'next/link'
+
 
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <Link href={'/dashboard'}><Button>Dashoard</Button></Link>
   );
 }
